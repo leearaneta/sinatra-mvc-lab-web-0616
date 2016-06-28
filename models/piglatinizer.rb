4d3d0076b04d25d9f316802e04c67bc627f0cd04
leearaneta <lee.araneta@gmail.com>
2016-06-28 15:16:06 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,7 @@ class PigLatinizer
   def piglatinize(string)
     letters = ""
     letters += string.slice!(0) until string.slice(0).scan(/[^aeiou]/i).count == 0
-    letters == "" ? new_string = string + "way" : new_string = string + letters + "ay"
+    letters.empty? ? new_string = string + "way" : new_string = string + letters + "ay"
   end
 
   def to_pig_latin(string)
